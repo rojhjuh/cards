de658ae0e6bcaaa67de43e25cbf46ad04ba26957
DmitryTell <dmitrij.teleganov@gmail.com>
2023-07-07 22:05:14 +0300

    HW-3 complete

---
+++ src/cards.js
@@ -0,0 +1,182 @@
+export const cards = [
+    {
+        rank: 'A',
+        suitCenter: './assets/img/peaks-center.png',
+        suitCorner: './assets/img/peaks-corner.png',
+    },
+    {
+        rank: 'K',
+        suitCenter: './assets/img/peaks-center.png',
+        suitCorner: './assets/img/peaks-corner.png',
+    },
+    {
+        rank: 'Q',
+        suitCenter: './assets/img/peaks-center.png',
+        suitCorner: './assets/img/peaks-corner.png',
+    },
+    {
+        rank: 'J',
+        suitCenter: './assets/img/peaks-center.png',
+        suitCorner: './assets/img/peaks-corner.png',
+    },
+    {
+        rank: '10',
+        suitCenter: './assets/img/peaks-center.png',
+        suitCorner: './assets/img/peaks-corner.png',
+    },
+    {
+        rank: '9',
+        suitCenter: './assets/img/peaks-center.png',
+        suitCorner: './assets/img/peaks-corner.png',
+    },
+    {
+        rank: '8',
+        suitCenter: './assets/img/peaks-center.png',
+        suitCorner: './assets/img/peaks-corner.png',
+    },
+    {
+        rank: '7',
+        suitCenter: './assets/img/peaks-center.png',
+        suitCorner: './assets/img/peaks-corner.png',
+    },
+    {
+        rank: '6',
+        suitCenter: './assets/img/peaks-center.png',
+        suitCorner: './assets/img/peaks-corner.png',
+    },
+    {
+        rank: 'A',
+        suitCenter: './assets/img/hearts-center.png',
+        suitCorner: './assets/img/hearts-corner.png',
+    },
+    {
+        rank: 'K',
+        suitCenter: './assets/img/hearts-center.png',
+        suitCorner: './assets/img/hearts-corner.png',
+    },
+    {
+        rank: 'Q',
+        suitCenter: './assets/img/hearts-center.png',
+        suitCorner: './assets/img/hearts-corner.png',
+    },
+    {
+        rank: 'J',
+        suitCenter: './assets/img/hearts-center.png',
+        suitCorner: './assets/img/hearts-corner.png',
+    },
+    {
+        rank: '10',
+        suitCenter: './assets/img/hearts-center.png',
+        suitCorner: './assets/img/hearts-corner.png',
+    },
+    {
+        rank: '9',
+        suitCenter: './assets/img/hearts-center.png',
+        suitCorner: './assets/img/hearts-corner.png',
+    },
+    {
+        rank: '8',
+        suitCenter: './assets/img/hearts-center.png',
+        suitCorner: './assets/img/hearts-corner.png',
+    },
+    {
+        rank: '7',
+        suitCenter: './assets/img/hearts-center.png',
+        suitCorner: './assets/img/hearts-corner.png',
+    },
+    {
+        rank: '6',
+        suitCenter: './assets/img/hearts-center.png',
+        suitCorner: './assets/img/hearts-corner.png',
+    },
+    {
+        rank: 'A',
+        suitCenter: './assets/img/diamonds-center.png',
+        suitCorner: './assets/img/diamonds-corner.png',
+    },
+    {
+        rank: 'K',
+        suitCenter: './assets/img/diamonds-center.png',
+        suitCorner: './assets/img/diamonds-corner.png',
+    },
+    {
+        rank: 'Q',
+        suitCenter: './assets/img/diamonds-center.png',
+        suitCorner: './assets/img/diamonds-corner.png',
+    },
+    {
+        rank: 'J',
+        suitCenter: './assets/img/diamonds-center.png',
+        suitCorner: './assets/img/diamonds-corner.png',
+    },
+    {
+        rank: '10',
+        suitCenter: './assets/img/diamonds-center.png',
+        suitCorner: './assets/img/diamonds-corner.png',
+    },
+    {
+        rank: '9',
+        suitCenter: './assets/img/diamonds-center.png',
+        suitCorner: './assets/img/diamonds-corner.png',
+    },
+    {
+        rank: '8',
+        suitCenter: './assets/img/diamonds-center.png',
+        suitCorner: './assets/img/diamonds-corner.png',
+    },
+    {
+        rank: '7',
+        suitCenter: './assets/img/diamonds-center.png',
+        suitCorner: './assets/img/diamonds-corner.png',
+    },
+    {
+        rank: '6',
+        suitCenter: './assets/img/diamonds-center.png',
+        suitCorner: './assets/img/diamonds-corner.png',
+    },
+    {
+        rank: 'A',
+        suitCenter: './assets/img/clubs-center.png',
+        suitCorner: './assets/img/clubs-corner.png',
+    },
+    {
+        rank: 'K',
+        suitCenter: './assets/img/clubs-center.png',
+        suitCorner: './assets/img/clubs-corner.png',
+    },
+    {
+        rank: 'Q',
+        suitCenter: './assets/img/clubs-center.png',
+        suitCorner: './assets/img/clubs-corner.png',
+    },
+    {
+        rank: 'J',
+        suitCenter: './assets/img/clubs-center.png',
+        suitCorner: './assets/img/clubs-corner.png',
+    },
+    {
+        rank: '10',
+        suitCenter: './assets/img/clubs-center.png',
+        suitCorner: './assets/img/clubs-corner.png',
+    },
+    {
+        rank: '9',
+        suitCenter: './assets/img/clubs-center.png',
+        suitCorner: './assets/img/clubs-corner.png',
+    },
+    {
+        rank: '8',
+        suitCenter: './assets/img/clubs-center.png',
+        suitCorner: './assets/img/clubs-corner.png',
+    },
+    {
+        rank: '7',
+        suitCenter: './assets/img/clubs-center.png',
+        suitCorner: './assets/img/clubs-corner.png',
+    },
+    {
+        rank: '6',
+        suitCenter: './assets/img/clubs-center.png',
+        suitCorner: './assets/img/clubs-corner.png',
+    },
+];
--- src/components/game-page-component.js
+++ src/components/game-page-component.js
@@ -1,18 +1,48 @@
+import { cards } from '../cards.js';
+
 export function renderGamePage({ gameElement }) {
+    let isOpenedCards = false;
+
+    const notOpenedCardsHtml = cards
+        .map(() => {
+            return `
+            <div class="card card--shirt"></div>`;
+        })
+        .join('');
+
+    const openedCardsHtml = cards
+        .map((card) => {
+            return `
+            <div class="card">
+                <div class="card__face face">
+                    <div class="face__up">
+                        <span class="face__rank">${card.rank}</span>
+                        <img src="${card.suitCorner}">
+                    </div>
+                    <img src="${card.suitCenter}">
+                    <div class="face__down">
+                        <span class="face__rank">${card.rank}</span>
+                        <img src="${card.suitCorner}">
+                    </div>
+                </div>
+            </div>`;
+        })
+        .join('');
+
     gameElement.innerHTML = `
         <div class="modal game-page">
             <div class="header">
                 <div class="header__timer timer">
                     <div class="timer__names">
-                        <p class="timer__name">min</p>
-                        <p class="timer__name">sek</p>
+                        <span class="timer__name">min</span>
+                        <span class="timer__name">sek</span>
                     </div>
                     <p class="timer__digits">00.00</p>
                 </div>
                 <button class="button" id="restart">Начать заново</button>
             </div>
             <div class="cards">
-            
+                ${isOpenedCards ? openedCardsHtml : notOpenedCardsHtml}
             </div>
         </div>`;
 }
